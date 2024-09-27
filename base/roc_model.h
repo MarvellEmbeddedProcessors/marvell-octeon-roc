4f05981b55a9917627720da9ba7e260436bc6e93
Monendra Singh Kushwaha <kmonendra@marvell.com>
2024-09-28 00:48:14 +0530

    common/cnxk: add CN96xx D0 model
    Adding support for CN96xx D0 pass.
    
    Change-Id: Iab3ae6f0bd5f9c7ec5c336f19ac29b7d54fc8fca
    Signed-off-by: Harman Kalra <hkalra@marvell.com>

--- base/roc_model.h
+++ base/roc_model.h
@@ -15,6 +15,7 @@ struct roc_model {
 #define ROC_MODEL_CN96xx_A0    BIT_ULL(0)
 #define ROC_MODEL_CN96xx_B0    BIT_ULL(1)
 #define ROC_MODEL_CN96xx_C0    BIT_ULL(2)
+#define ROC_MODEL_CN96xx_D0    BIT_ULL(3)
 #define ROC_MODEL_CNF95xx_A0   BIT_ULL(4)
 #define ROC_MODEL_CNF95xx_B0   BIT_ULL(6)
 #define ROC_MODEL_CNF95xxMM_A0 BIT_ULL(8)
@@ -46,8 +47,8 @@ struct roc_model {
 #define ROC_MODEL_CN96xx_Ax (ROC_MODEL_CN96xx_A0 | ROC_MODEL_CN96xx_B0)
 #define ROC_MODEL_CN98xx_Ax (ROC_MODEL_CN98xx_A0 | ROC_MODEL_CN98xx_A1)
 #define ROC_MODEL_CN9K                                                         \
-	(ROC_MODEL_CN96xx_Ax | ROC_MODEL_CN96xx_C0 | ROC_MODEL_CNF95xx_A0 |    \
-	 ROC_MODEL_CNF95xx_B0 | ROC_MODEL_CNF95xxMM_A0 |                       \
+	(ROC_MODEL_CN96xx_Ax | ROC_MODEL_CN96xx_C0 | ROC_MODEL_CN96xx_D0      |\
+	 ROC_MODEL_CNF95xx_A0 | ROC_MODEL_CNF95xx_B0 | ROC_MODEL_CNF95xxMM_A0 |\
 	 ROC_MODEL_CNF95xxO_A0 | ROC_MODEL_CNF95xxN_A0 | ROC_MODEL_CN98xx_Ax | \
 	 ROC_MODEL_CNF95xxN_A1 | ROC_MODEL_CNF95xxN_B0)
 #define ROC_MODEL_CNF9K                                                        \
@@ -155,6 +156,12 @@ static inline uint64_t
 roc_model_is_cn96_cx(void)
 {
 	return (roc_model->flag & ROC_MODEL_CN96xx_C0);
+}
+
+static inline uint64_t
+roc_model_is_cn96_dx(void)
+{
+	return (roc_model->flag & ROC_MODEL_CN96xx_D0);
 }
 
 static inline uint64_t
