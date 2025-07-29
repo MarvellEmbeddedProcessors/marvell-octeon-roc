2e41d291f08555f5be590dda78afe4a2915bcc1d
Monendra Singh Kushwaha <kmonendra@marvell.com>
2025-07-29 11:33:31 +0530

    common/cnxk: get context ilen as devarg in cn20k
    In cn20k, receive cpt context ilen as devarg parameter
    and configure lf.
    And update crypto documentation.
    
    Signed-off-by: Nithinsen Kaithakadan <nkaithakadan@marvell.com>
    Change-Id: I956b2af347f2286f55e5e07a5ef68d2bd07b384c
    Reviewed-on: https://sj1git1.cavium.com/c/IP/SW/dataplane/dpdk/+/155633
    Base-Builds: sa_ip-toolkits-Jenkins <sa_ip-toolkits-jenkins@marvell.com>
    Base-Tests: sa_ip-toolkits-Jenkins <sa_ip-toolkits-jenkins@marvell.com>
    Tested-by: sa_ip-toolkits-Jenkins <sa_ip-toolkits-jenkins@marvell.com>
    Reviewed-by: Anoob Joseph <anoobj@marvell.com>
    (cherry picked from commit 6c78008a0a5bb585534a5e808469d281aad0c9ea)
    Reviewed-on: https://sj1git1.cavium.com/c/IP/SW/dataplane/dpdk/+/158722

--- base/roc_cpt.c
+++ base/roc_cpt.c
@@ -639,6 +639,9 @@ roc_cpt_dev_configure(struct roc_cpt *roc_cpt, int nb_lf, bool rxc_ena, uint16_t
 		ctx_ilen_valid = true;
 		/* Inbound SA size is max context size */
 		ctx_ilen = (PLT_ALIGN(ROC_OT_IPSEC_SA_SZ_MAX, ROC_ALIGN) / 128) - 1;
+	} else if (roc_cpt->ctx_ilen != 0) {
+		ctx_ilen = roc_cpt->ctx_ilen;
+		ctx_ilen_valid = true;
 	}
 
 	rc = cpt_lfs_alloc(&cpt->dev, eng_grpmsk, blkaddr[blknum], false, ctx_ilen_valid, ctx_ilen,
