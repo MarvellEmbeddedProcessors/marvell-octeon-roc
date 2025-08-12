cb2e34fbc4d267f659bac31a9e223238c4e9216f
Monendra Singh Kushwaha <kmonendra@marvell.com>
2025-08-12 23:29:47 +0530

    common/cnxk: add support to trigger virto config msix
    Adds support for triggering the Virtio configuration change
    MSI-X interrupt.
    
    Change-Id: I77e8949c5a6ca719e83a3f6e8706299013417cc9
    Signed-off-by: Srujana Challa <schalla@marvell.com>

--- base/roc_emdev.h
+++ base/roc_emdev.h
@@ -206,6 +206,7 @@ int __roc_api roc_emdev_irqs_register(struct roc_emdev *roc_emdev);
 void __roc_api roc_emdev_irqs_unregister(struct roc_emdev *roc_emdev);
 int __roc_api roc_emdev_mbox_msix_cfg(struct roc_emdev *roc_emdev, uint16_t evf_id,
 				      uint16_t mbox_msix);
+void __roc_api roc_emdev_psw_mbox_int_trigger(struct roc_emdev *roc_emdev, uint16_t evf_id);
 
 /* Debug APIs */
 int __roc_api roc_emdev_psw_anq_desc_dump(FILE *file, void *data);
