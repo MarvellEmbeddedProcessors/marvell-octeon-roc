b49ffb4cc462d32822907cb20d68cdaa6f870885
Monendra Singh Kushwaha <kmonendra@marvell.com>
2025-08-12 23:10:25 +0530

    common/cnxk: add O20 DPI support
    Adds support for O20 DPI hardware.
    
    Signed-off-by: Vamsi Attunuru <vattunuru@marvell.com>
    Change-Id: I903adc41a97f5d15b334497fa1fde5ce7e3932c4
    Reviewed-on: https://sj1git1.cavium.com/c/IP/SW/dataplane/dpdk/+/146993
    Base-Builds: sa_ip-toolkits-Jenkins <sa_ip-toolkits-jenkins@marvell.com>
    Tested-by: sa_ip-toolkits-Jenkins <sa_ip-toolkits-jenkins@marvell.com>
    Tested-by: Jerin Jacob <jerinj@marvell.com>
    Reviewed-by: Nithin Kumar Dabilpuram <ndabilpuram@marvell.com>
    Reviewed-by: Jerin Jacob <jerinj@marvell.com>

--- base/roc_dpi_priv.h
+++ base/roc_dpi_priv.h
@@ -43,4 +43,36 @@ typedef union dpi_mbox_msg_t {
 	} s;
 } dpi_mbox_msg_t;
 
+struct dpi {
+	struct plt_pci_device *pci_dev;
+	struct dev dev;
+	uint16_t lf_msix_off[ROC_DPI_MAX_LFS];
+	uint8_t lf_blkaddr[ROC_DPI_MAX_LFS];
+};
+
+static inline struct dpi *
+roc_dpi_to_dpi_priv(struct roc_dpi *roc_dpi)
+{
+	return (struct dpi *)&roc_dpi->reserved[0];
+}
+
+static inline struct roc_dpi *
+dpi_priv_to_roc_dpi(struct dpi *dpi)
+{
+	return (struct roc_dpi *)((char *)dpi - offsetof(struct roc_dpi, reserved));
+}
+
+int dpi_lf_reset(struct roc_dpi_lf *lf);
+void dpi_lf_ena_dis(struct roc_dpi_lf *lf, uint8_t enb);
+int dpi_lfs_attach(struct dev *dev, uint8_t blkaddr, bool modify, uint16_t nb_lf);
+int dpi_lfs_detach(struct dev *dev);
+int dpi_lf_attach(struct dev *dev, uint8_t blkaddr, bool modify, uint16_t nb_lf);
+int dpi_lf_detach(struct dev *dev);
+int dpi_lf_init(struct roc_dpi_lf *lf, struct dev *dev, uint8_t slot);
+int dpi_chan_tbl_alloc(struct dev *dev, uint8_t blk_addr, uint16_t tbl_sz);
+int dpi_chan_tbl_free(struct dev *dev, uint8_t blk_addr, uint16_t tbl_num);
+int dpi_chan_tbl_ena_dis(struct dev *dev, uint16_t lfid, uint16_t chan_tbl, bool enable);
+int dpi_chan_tbl_update(struct dev *dev, uint8_t blk_addr, uint16_t chan_tbl, uint64_t *tbl,
+			uint16_t off, uint16_t nb_entries);
+
 #endif
