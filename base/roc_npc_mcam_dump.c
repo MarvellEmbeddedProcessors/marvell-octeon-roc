aa16ab30e7be6970170b3b947cfb1e1cdb5d5855
Monendra Singh Kushwaha <kmonendra@marvell.com>
2026-01-01 17:13:05 +0530

    drivers: replace packed attributes
    MSVC struct packing is not compatible with GCC. Replace macro
    __rte_packed with __rte_packed_begin to push existing pack value
    and set packing to 1-byte and macro __rte_packed_end to restore
    the pack value prior to the push.
    
    Macro __rte_packed_end is deliberately utilized to trigger a
    MSVC compiler warning if no existing packing has been pushed allowing
    easy identification of locations where the __rte_packed_begin is
    missing.
    
    Signed-off-by: Andre Muezerie <andremue@linux.microsoft.com>
    Reviewed-by: Tyler Retzlaff <roretzla@linux.microsoft.com>

--- base/roc_npc_mcam_dump.c
+++ base/roc_npc_mcam_dump.c
@@ -62,7 +62,7 @@ union npc_rx_parse_nibble_cn20k_u {
 	uint64_t u;
 };
 
-struct npc_rx_parse_nibble_s {
+struct __plt_packed_begin npc_rx_parse_nibble_s {
 	uint16_t chan : 3;
 	uint16_t errlev : 1;
 	uint16_t errcode : 2;
@@ -83,7 +83,7 @@ struct npc_rx_parse_nibble_s {
 	uint16_t lgtype : 1;
 	uint16_t lhflags : 2;
 	uint16_t lhtype : 1;
-} __plt_packed;
+} __plt_packed_end;
 
 static const char *const intf_str[] = {
 	"NIX-RX",
