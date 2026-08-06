9195f08760e95f18d455248e049d5383ca5c52a8
Ashwin Sekhar T K <asekhar@marvell.com>
2026-08-06 07:52:38 +0000

    common/cnxk: define mbox for cn20k npa cache sync
    In cn20k, NPA doesn't have cache in NDC, it has its own cache inside
    block itself. This commit defines the mbox for syncing the npa cache.
    The mbox request is supposed to populate NPA_AF_CACHE_SYNC register that
    would sync the cache.
    
    Signed-off-by: Ashwin Sekhar T K <asekhar@marvell.com>
    Cherry-picked-from-dpdk: 3c9fc365fa4bacfa669f412ed5a6be3689aa05ef

--- base/roc_nix_inl.c
+++ base/roc_nix_inl.c
@@ -2621,7 +2621,7 @@ roc_nix_inl_ctx_write(struct roc_nix *roc_nix, void *sa_dptr, void *sa_cptr,
 		return -EINVAL;
 
 	if (roc_nix) {
-		if (inb && roc_nix->custom_inb_sa && sa_len > ROC_NIX_INL_INB_CUSTOM_SA_SZ) {
+		if (inb && roc_nix->custom_inb_sa && sa_len > ROC_NIX_INL_INB_CUSTOM_SA_SZ)
 			plt_nix_dbg("SA length: %u is more than allocated length: %u\n", sa_len,
 				    ROC_NIX_INL_INB_CUSTOM_SA_SZ);
 		nix = roc_nix_to_nix_priv(roc_nix);
